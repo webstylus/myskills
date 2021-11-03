e17600e816b62795987cabc519528f26d62506af
Rafael Pacífico <rafael@webstylus.com.br>
2021-11-03 14:14:09 -0300

    Finish procced to publish play store

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaView, StatusBar } from 'react-native'
 import { Home } from './src/pages/Home'
+import SplashScreen from 'react-native-splash-screen'
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar barStyle={'light-content'} backgroundColor={'#121015'} />
+      <StatusBar barStyle={'light-content'} />
       <Home />
     </SafeAreaView>
   )
