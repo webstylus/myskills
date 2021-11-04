eb48b494507a62e92a279222cf6ecc121548c223
Rafael Pacífico <rafael@webstylus.com.br>
2021-11-03 23:01:51 -0300

    Rollback

--- android/app/src/main/java/com/myskills/MainActivity.java
+++ android/app/src/main/java/com/myskills/MainActivity.java
@@ -1,4 +1,4 @@
-package com.webstylus.myskills;
+package com.myskills;
 
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
@@ -17,6 +17,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "MySkills";
+    return "myskills";
   }
 }
